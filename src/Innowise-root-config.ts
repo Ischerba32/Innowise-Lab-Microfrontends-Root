7d9b0dc8a761723cb0ae7bc6cf3726b3f13eb95c
Igor Scherba <i.scherba32@gmail.com>
2022-07-04 15:50:14 +0300

    Code formatting

--- src/Innowise-root-config.ts
+++ src/Innowise-root-config.ts
@@ -1,38 +1,20 @@
-import { registerApplication, start } from "single-spa";
+import { registerApplication, start } from 'single-spa';
 import {
-  constructApplications,
-  constructRoutes,
-  constructLayoutEngine,
-} from "single-spa-layout";
-import microfrontendLayout from "./microfrontend-layout.html";
-import * as isActive from "./activity-functions";
+	constructApplications,
+	constructRoutes,
+	constructLayoutEngine,
+} from 'single-spa-layout';
+import microfrontendLayout from './microfrontend-layout.html';
+import * as isActive from './activity-functions';
 
 const routes = constructRoutes(microfrontendLayout);
 const applications = constructApplications({
-  routes,
-  loadApp({ name }) {
-    return System.import(name);
-  },
+	routes,
+	loadApp({ name }) {
+		return System.import(name);
+	},
 });
 const layoutEngine = constructLayoutEngine({ routes, applications });
-
-// registerApplication(
-//   "@Innowise/home",
-//   () => System.import("@Innowise/home"),
-//   isActive.home
-// );
-
-// registerApplication(
-//   "@Innowise/todo",
-//   () => System.import("@Innowise/todo"),
-//   isActive.todo
-// );
-
-// registerApplication(
-//   "@Innowise/mini-paint",
-//   () => System.import("@Innowise/mini-paint"),
-//   isActive.mini_paint
-// );
 
 applications.forEach(registerApplication);
 layoutEngine.activate();
--- src/activity-functions.ts
+++ src/activity-functions.ts
@@ -1,18 +1,18 @@
 export function prefix(location, ...prefixes) {
-  return prefixes.some(
-    (prefix) => location.href.indexOf(`${location.origin}/${prefix}`) !== -1
-  );
+	return prefixes.some(
+		(prefix) => location.href.indexOf(`${location.origin}/${prefix}`) !== -1
+	);
 }
 
 export function home() {
-  // The nav is always active
-  return prefix(location, "");
+	// The nav is always active
+	return prefix(location, '');
 }
 
 export function todo(location) {
-  return prefix(location, "todo");
+	return prefix(location, 'todo');
 }
 
 export function mini_paint(location) {
-  return prefix(location, "mini-paint");
+	return prefix(location, 'mini-paint');
 }
